da780dc424e7fdf549c1e77ad45a6876ea202554
Jose Quiceno <divina.familia.nostri@gmail.com>
2025-04-22 04:05:16 -0500

    feat(app): initialize working streamlit app
    - Add page setup
    - Get total, tip_percent, and people as inputs
    - Output split cost

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(page_title="Lots", page_icon="⛃")
+st.title("Lots: Tip Calculator")
+st.caption("Split the bill equitably.")
+
+total = st.number_input("What was the total bill?", min_value=0.0, step=0.01, format="%.2f")
+tip_percent = st.selectbox("How much tip would you like to give?", [10, 12, 15, 20, 25])
+people = st.number_input("How many people to split the bill?", min_value=1, step=1)
+
+if total > 0:
+    tip_amount = total * (tip_percent / 100)
+    total_with_tip = total + tip_amount
+    split_amount = total_with_tip / people
+
+    st.subheader("Each person should pay:")
+    st.success(f"${split_amount:.2f}")
